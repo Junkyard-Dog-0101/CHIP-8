74eb03280a0589fcc944c5400bf84100bb26ac3f
Julien Vesy <julien.vesy@epitech.eu>
2013-10-25 23:59:17 +0200

    event face

--- inc/SDLmanager.h
+++ inc/SDLmanager.h
@@ -3,9 +3,10 @@
 
 #include <SDL/SDL.h>
 
-#define MULTI 4
+#define MULTI 5
 
 int	loadSDL(SDL_Surface *screen, SDL_Surface *pixel[2048], SDL_Rect *location);
 int	unloadSDL(SDL_Surface *pixel[2048]);
+int	eventSDL(SDL_Event *event);
 
 #endif
--- src/SDLmanager.c
+++ src/SDLmanager.c
@@ -18,7 +18,7 @@ int	loadSDL(SDL_Surface *screen, SDL_Surface *pixel[2048], SDL_Rect *location)
       for (x = 0; x < (64 * MULTI); x = x + MULTI)
 	{
 	  location->x = x;
-	  if (y % 2)
+	  if (x % 2)
 	    SDL_FillRect(pixel[i], NULL, SDL_MapRGB(screen->format, 255, 255, 255));
 	  else
 	    SDL_FillRect(pixel[i], NULL, SDL_MapRGB(screen->format, 0, 0, 0));
@@ -37,4 +37,71 @@ int	unloadSDL(SDL_Surface *pixel[2048])
   for (i = 0; i < 2048; ++i) 
     SDL_FreeSurface(pixel[i]);
   SDL_Quit();
+  return (0);
+}
+
+int	eventSDL(SDL_Event *event)
+{
+  if ((SDL_PollEvent(event) != 0) && (event->type == SDL_KEYDOWN))
+    {
+      switch (event->key.keysym.sym)
+	{
+	  /* |1|2|3|C| */
+	case SDLK_1:
+	  //
+	  break;
+	case SDLK_2:
+	  //
+	  break;
+	case SDLK_3:
+	  //
+	  break;
+	case SDLK_4:
+	  //
+	  break;
+
+	  /* |4|5|6|D| */
+	case SDLK_a:
+	  //
+	  break;
+	case SDLK_z:
+	  //
+	  break;
+	case SDLK_e:
+	  //
+	  break;
+	case SDLK_r:
+	  //
+	  break;
+
+	  /* |7|8|9|E| */
+	case SDLK_q:
+	  //
+	  break;
+	case SDLK_s:
+	  //
+	  break;
+	case SDLK_d:
+	  //
+	  break;
+	case SDLK_f:
+	  //
+	  break;
+
+	  /* |A|0|B|F| */
+	case SDLK_w:
+	  //
+	  break;
+	case SDLK_x:
+	  //
+	  break;
+	case SDLK_c:
+	  //
+	  break;
+	case SDLK_v:
+	  //
+	  break;
+	}
+    }
+  return (0);
 }
